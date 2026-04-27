42dcb8621140a2c8b9665bed6e488a2a77b24a75
zhnuraa <250822@astanait.edu.kz>
2026-04-27 08:07:38 +0500

    Implement basic and advanced sorting algorithms

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,1 +1,72 @@
+import java.util.Random;
 
+public class Sorter {
+
+    public void basicSort(int[] arr) {
+        // Insertion Sort
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public void advancedSort(int[] arr) {
+        // Quick Sort
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public int[] generateRandomArray(int size) {
+        Random random = new Random();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(1000);
+        }
+
+        return arr;
+    }
+}
