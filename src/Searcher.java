e912f119aa96dbd0318593cc9bbb27c4f244ef19
zhnuraa <250822@astanait.edu.kz>
2026-04-27 08:07:59 +0500

    Update Searcher.java

--- src/Searcher.java
+++ src/Searcher.java
@@ -1,1 +1,22 @@
+public class Searcher {
 
+    public int search(int[] arr, int target) {
+        // Binary Search
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
